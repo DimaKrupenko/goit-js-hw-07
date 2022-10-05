39305a73ccc46604c19e481a177c9d62e5483d09
DimaKrupenko <dimakrupenko77@gmail.com>
2022-10-05 15:50:20 +0300

    сделал первое задание

--- src 2/js/01-gallery.js
+++ src 2/js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const itemEl = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+            <a class="gallery__link" href=${item.original} onclick="return false;">
+            <img class="gallery__image" src=${item.preview} data-source=${item.original} alt='${item.description}'></a>
+        </div> `
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", itemEl);
+
+gallery.addEventListener("click", onClickPicture);
+
+function onClickPicture(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    
+        <img src=${evt.target.getAttribute("data-source")} >
+`);
+
+  instance.show();
+}
